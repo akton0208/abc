86e0ed602ee5b1acc2038361961062ab470417da
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-12-02 00:57:48 +0800

    Add files via upload

--- core/grass.py
+++ core/grass.py
@@ -47,8 +47,7 @@ class Grass(GrassWs, GrassRest, FailureCounter):
         self.limit = 7
 
     async def start(self):
-        if self.db:
-            self.proxies = await self.db.get_proxies_by_email(self.email)
+        self.proxies = await self.db.get_proxies_by_email(self.email)
         self.log_global_count(True)
         # logger.info(f"{self.id} | {self.email} | Starting...")
         while True:
@@ -95,16 +94,13 @@ class Grass(GrassWs, GrassRest, FailureCounter):
         while True:
             try:
                 await self.connection_handler()
-
-                await self.auth_to_extension(browser_id, user_id)
+                await self.auth_to_desktop(browser_id, user_id)
 
                 if self.proxy_score is None:
                     await asyncio.sleep(1)
 
                     if MIN_PROXY_SCORE:
                         await self.handle_proxy_score(MIN_PROXY_SCORE)
-
-                    await self.handle_http_request_action()
 
                 for i in range(10 ** 9):
                     await self.send_ping()
@@ -126,17 +122,16 @@ class Grass(GrassWs, GrassRest, FailureCounter):
                     if i:
                         self.fail_reset()
 
-                    await asyncio.sleep(random.randint(119, 120))
-            except (WebsocketClosedException, ConnectionResetError, TypeError) as e:
-                logger.info(f"{self.id} | {type(e).__name__}: {e}. Reconnecting...")
-            # except ConnectionResetError as e:
-            #     logger.info(f"{self.id} | Connection reset: {e}. Reconnecting...")
-            # except TypeError as e:
-            #     logger.info(f"{self.id} | Type error: {e}. Reconnecting...")
-                # await self.delay_with_log(msg=f"{self.id} | Reconnecting with delay for some minutes...", sleep_time=60)
-            # except Exception as e:
-            #     logger.info(f"{self.id} | {traceback.format_exc()}")
-            await self.failure_handler(limit=2)
+                    await asyncio.sleep(5)
+            except WebsocketClosedException as e:
+                logger.info(f"{self.id} | Websocket closed: {e}. Reconnecting...")
+            except ConnectionResetError as e:
+                logger.info(f"{self.id} | Connection reset: {e}. Reconnecting...")
+            except TypeError as e:
+                logger.info(f"{self.id} | Type error: {e}. Reconnecting...")
+                await self.delay_with_log(msg=f"{self.id} | Reconnecting with delay for some minutes...", sleep_time=60)
+
+            await self.failure_handler(limit=4)
 
             await asyncio.sleep(5, 10)
 
@@ -146,7 +141,7 @@ class Grass(GrassWs, GrassRest, FailureCounter):
 
         logger.info(f"{self.id} | Claimed all rewards.")
 
-    @retry(stop=stop_after_attempt(7),
+    @retry(stop=stop_after_attempt(12),
            retry=(retry_if_exception_type(ConnectionError) | retry_if_not_exception_type(ProxyForbiddenException)),
            retry_error_callback=lambda retry_state:
            raise_error(WebsocketConnectionFailedError(f"{retry_state.outcome.exception()}")),
@@ -163,7 +158,7 @@ class Grass(GrassWs, GrassRest, FailureCounter):
            wait=wait_random(5, 7),
            reraise=True)
     async def handle_proxy_score(self, min_score: int):
-        if (proxy_score := await self.get_proxy_score_via_devices_by_device_handler()) is None:
+        if (proxy_score := await self.get_proxy_score_by_device_id_handler()) is None:
             # logger.info(f"{self.id} | Proxy score not found for {self.proxy}. Guess Bad proxies! Continue...")
             # return None
             raise ProxyScoreNotFoundException(f"{self.id} | Proxy score not found! Retrying...")
